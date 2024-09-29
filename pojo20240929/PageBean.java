06d44239080108e7b85706bf72a58945494f4e09
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-09-29 23:53:35 +0800

    Add files via upload
    Springboot pojo 2024.9.29

---
+++ pojo20240929/PageBean.java
@@ -0,0 +1,15 @@
+package com.wzb.pojo20240929;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean {
+    private Long total;
+    private List<Emp> rows;
+}
