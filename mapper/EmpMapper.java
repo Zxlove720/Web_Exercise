081319d37981c35730f08bd150f429a87c0b2e15
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-09-30 23:31:41 +0800

    Add files via upload
    Springboot mapper Exercise 2024.9.30

--- mapper/EmpMapper.java
+++ mapper/EmpMapper.java
@@ -1,8 +1,13 @@
 package com.wzb.mapper;
 
+import com.wzb.pojo20240930.Emp;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface EmpMapper {
 
+    List<Emp> pageSelect(String name, Short gender, LocalDate begin, LocalDate end);
 }
