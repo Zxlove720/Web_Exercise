c4b44c6b0107bbdd92c96410eb9c5e19819720f4
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-09-30 23:31:25 +0800

    Add files via upload
    Springboot service Exercise 2024.9.30

--- service/EmpService.java
+++ service/EmpService.java
@@ -1,4 +1,12 @@
 package com.wzb.service;
 
+import com.wzb.pojo20240930.Emp;
+import com.wzb.pojo20240930.PageBean;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface EmpService {
+
+    PageBean pageSelect(Integer page, Integer pageSize, String name, Short gender, LocalDate begin, LocalDate end);
 }
